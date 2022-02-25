f9797466c251bd8c12645b16b4622ca3bc084089
Goran Bogovac <=>
2022-02-25 19:18:25 +0100

    and one

---
+++ AlgoPractice/Arrays/PlusOneSolution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoPractice.Arrays
+{
+    public class PlusOneSolution
+    {
+        /// <summary>
+        /// You are given a large integer represented as an integer array digits, 
+        /// where each digits[i] is the ith digit of the integer. 
+        /// The digits are ordered from most significant to least significant in left-to-right order. 
+        /// The large integer does not contain any leading 0's.
+        /// Increment the large integer by one and return the resulting array of digits.
+        /// </summary>
+        /// <param name="digits"></param>
+        /// <returns></returns>
+        public int[] PlusOne(int[] digits)
+        {
+            if (digits == null || digits.Length == 0)
+                return Array.Empty<int>();
+
+            var result = new List<int>();
+            bool addedOne = false;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] == 9 && !addedOne)
+                {
+                    result.Add(0);
+                }
+                else if (!addedOne)
+                {
+                    result.Add(digits[i] + 1);
+                    addedOne = true;
+                }
+                else
+                {
+                    result.Add(digits[i]);
+                }
+            }
+
+            if (!addedOne)
+            {
+                result.Add(1);
+            }
+
+            result.Reverse();
+            return result.ToArray();
+        }
+    }
+}
+
---
+++ AlgoPracticeTest/Arrays/PlusOneTest.cs
@@ -0,0 +1,21 @@
+using Xunit;
+using AlgoPractice.Arrays;
+using System;
+
+namespace AlgoPracticeTest.Arrays;
+
+public class PlusOneTest
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 2, 4 })]
+    [InlineData(new int[] { 4, 3, 2, 1 }, new int[] { 4, 3, 2, 2 })]
+    [InlineData(new int[] { 9 }, new int[] { 1, 0 })]
+    [InlineData(new int[] { 3, 9, 9 }, new int[] { 4, 0, 0 })]
+    [InlineData(new int[] { 2, 3, 9, 9 }, new int[] { 2, 4, 0, 0 })]
+    public void TestPlusOne(int[] nums, int[] expectedResult)
+    {
+        var sln = new PlusOneSolution();
+        var result = sln.PlusOne(nums);
+        Assert.Equal(expectedResult, result);
+    }
+}
