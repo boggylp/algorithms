848fb8b3574b9ec7f9f23d18b8f0df087964bd58
Boggy <g.bogovac@outlook.com>
2022-03-01 00:52:13 +0100

    two sum bebo

---
+++ AlgoPracticeTest/Arrays/TwoSumTest.cs
@@ -0,0 +1,19 @@
+using Xunit;
+using AlgoPractice.Arrays;
+using System;
+
+namespace AlgoPracticeTest.Arrays;
+
+public class TwoSumTest
+{
+    [Theory]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+    [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+    [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+    public void TestTwoSum(int[] nums, int target, int[] expectedResult)
+    {
+        var algo = new TwoSumSolution();
+        var result = algo.TwoSum(nums, target);
+        Assert.Equal(expectedResult, result);
+    }
+}
