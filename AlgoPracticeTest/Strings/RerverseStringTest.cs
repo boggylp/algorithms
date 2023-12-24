a8df84864fd68e08f07fe4bf717205b1f1d87d6e
boggylp <7112376+boggylp@users.noreply.github.com>
2023-12-24 19:39:50 +0100

    Reverse string and editorconfig (#4)

---
+++ AlgoPracticeTest/Strings/RerverseStringTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AlgoPractice.Strings;
+using Xunit;
+
+namespace AlgoPracticeTest;
+
+public class RerverseStringTest
+{
+    [Theory]
+    [InlineData(new char[] { 'h', 'e', 'l', 'l', 'o' }, new char[] { 'o', 'l', 'l', 'e', 'h' })]
+    [InlineData(new char[] { 'H', 'a', 'n', 'n', 'a', 'h' }, new char[] { 'h', 'a', 'n', 'n', 'a', 'H' })]
+    public void ValidHappyFlow(char[] input, char[] expectedOutput)
+    {
+        ReverseString.Run(input);
+        Assert.Equivalent(expectedOutput, input);
+    }
+}
