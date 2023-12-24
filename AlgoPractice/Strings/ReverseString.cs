d4ac10958a385914a2872167097c5d47a5ba4502
Bogambe <bogambe@gmail.com>
2023-12-24 19:38:58 +0100

    Reverse string and editorconfig

---
+++ AlgoPractice/Strings/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgoPractice.Strings;
+
+public class ReverseString
+{
+    public static void Run(char[] characters)
+    {
+        int j = characters.Length - 1;
+        for (int i = 0; i == j; i++)
+        {
+            (characters[j], characters[i]) = (characters[i], characters[j]);
+        }
+    }
+}
