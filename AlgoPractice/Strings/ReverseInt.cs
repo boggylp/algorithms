2c430b271a4213aafb6ce806c4793b7400b50566
boggylp <7112376+boggylp@users.noreply.github.com>
2023-12-24 23:32:05 +0100

    Stackoverflow but it's ok (#5)
    Stackoverflow but it's ok

---
+++ AlgoPractice/Strings/ReverseInt.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgoPractice.Strings;
+
+public class ReverseInt
+{
+    public static int Run(int x)
+    {
+        if (x == int.MaxValue)
+            return int.MinValue;
+
+        char[] charArray = x.ToString().ToCharArray();
+        char[] invertedChar = charArray.Reverse().ToArray();
+
+        var prefix = charArray[0];
+        if (!char.IsDigit(prefix))
+        {
+            return int.Parse($"{prefix}{new string(invertedChar.Take(invertedChar.Length - 1).ToArray())}");
+        }
+
+        return int.Parse(invertedChar);
+    }
+}
