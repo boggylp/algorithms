80d27e69b8de07910aae4cccfab22f0d8e5ce934
Bogambe <bogambe@gmail.com>
2023-12-24 23:31:22 +0100

    solved

---
+++ AlgoPracticeTest/Strings/ReverseIntTest.cs
@@ -0,0 +1,17 @@
+﻿using AlgoPractice.Strings;
+using Xunit;
+
+namespace AlgoPracticeTest;
+
+public class RerverseIntTest
+{
+    [Theory]
+    [InlineData(123, 321)]
+    [InlineData(-123, -321)]
+    [InlineData(120, 21)]
+    public void ValidHappyFlow(int input, int expectedResult)
+    {
+        var result = ReverseInt.Run(input);
+        Assert.Equal(expectedResult, result);
+    }
+}
