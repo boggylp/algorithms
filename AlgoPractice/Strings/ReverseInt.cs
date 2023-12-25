0c224ec9b2750c55db4526475da78e6dd764da83
Bogambe <bogambe@gmail.com>
2023-12-25 17:42:29 +0100

    fix int32

--- AlgoPractice/Strings/ReverseInt.cs
+++ AlgoPractice/Strings/ReverseInt.cs
@@ -4,18 +4,12 @@ public class ReverseInt
 {
     public static int Run(int x)
     {
-        if (x == int.MaxValue)
-            return int.MinValue;
-
-        char[] charArray = x.ToString().ToCharArray();
-        char[] invertedChar = charArray.Reverse().ToArray();
-
-        var prefix = charArray[0];
-        if (!char.IsDigit(prefix))
-        {
-            return int.Parse($"{prefix}{new string(invertedChar.Take(invertedChar.Length - 1).ToArray())}");
-        }
-
-        return int.Parse(invertedChar);
+        string str = new string(x.ToString().Trim('-').Reverse().ToArray());
+        bool tryInt32 = int.TryParse(str, out int intValue);
+        return tryInt32
+            ? x.ToString().Contains('-')
+                ? intValue * -1
+                : intValue
+            : 0;
     }
 }
