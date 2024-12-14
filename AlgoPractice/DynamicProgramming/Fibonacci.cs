b7077e062d42ac306a122037994da93e76779168
Bogambe <bogambe@gmail.com>
2024-12-14 22:56:15 +0100

    Set up basic fibonacci

---
+++ AlgoPractice/DynamicProgramming/Fibonacci.cs
@@ -0,0 +1,17 @@
+﻿namespace AlgoPractice.DynamicProgramming;
+
+public static class Fibonacci
+{
+    public static int Calculate(int n, Dictionary<int, int>? memo = null)
+    {
+        memo ??= new Dictionary<int, int>();
+
+        if (n <= 2)
+            return 1;
+        if (memo.TryGetValue(n, out var result))
+            return result;
+
+        memo[n] = Calculate(n - 1, memo) + Calculate(n - 2, memo);
+        return memo[n];
+    }
+}
---
+++ AlgoPracticeTest/DynamicProgramming/FibonacciTest.cs
@@ -0,0 +1,20 @@
+﻿using AlgoPractice.DynamicProgramming;
+
+namespace AlgoPracticeTest.DynamicProgramming;
+
+public sealed class FibonacciTest
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(2, 1)]
+    [InlineData(0, 1)]
+    [InlineData(3, 2)]
+    [InlineData(4, 3)]
+    [InlineData(5, 5)]
+    [InlineData(6, 8)]
+    public void ValidHappyFlow(int input, int expectedResult)
+    {
+        var result = Fibonacci.Calculate(input);
+        Assert.Equal(expectedResult, result);
+    }
+}
