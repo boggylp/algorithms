371b433a6a0ce24607bf7a2dcd6f8de679cf6a79
Bogambe <bogambe@gmail.com>
2024-12-14 23:43:17 +0100

    Grid travel

---
+++ AlgoPractice/DynamicProgramming/GridTravel.cs
@@ -0,0 +1,39 @@
+﻿namespace AlgoPractice.DynamicProgramming;
+
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]).
+// The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+// The robot can only move either down or right at any point in time.
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+// Example 1
+// Input: m = 3, n = 7
+// Output: 28
+// Example 2
+// Input: m = 3, n = 2
+// Output: 3
+public static class GridTravel
+{
+    private record struct Point(int X, int Y);
+
+    public static int UniquePaths(int m, int n)
+    {
+        return UniquePaths(m, n, new Dictionary<Point, int>());
+    }
+
+    private static int UniquePaths(int m, int n, Dictionary<Point, int> results)
+    {
+        if (m <= 0 || n <= 0)
+            return 0;
+
+        if (m == 1 || n == 1)
+            return 1;
+
+        var point = new Point(m, n);
+        if (results.TryGetValue(point, out var result))
+            return result;
+
+        results[point] = UniquePaths(m - 1, n, results) + UniquePaths(m, n - 1, results);
+
+        return results[point];
+    }
+}
---
+++ AlgoPracticeTest/DynamicProgramming/GridTravelTest.cs
@@ -0,0 +1,16 @@
+﻿using AlgoPractice.DynamicProgramming;
+
+namespace AlgoPracticeTest.DynamicProgramming;
+
+public sealed class GridTravelTest
+{
+    [Theory]
+    [InlineData(3, 7, 28)]
+    [InlineData(3, 2, 3)]
+    [InlineData(23, 12, 193536720)]
+    public void ValidHappyFlow(int m, int n, int expectedResult)
+    {
+        var result = GridTravel.UniquePaths(m, n);
+        Assert.Equal(expectedResult, result);
+    }
+}
