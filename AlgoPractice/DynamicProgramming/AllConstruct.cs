4bf52d5298ddda6d0b1ef1c6b9d9859f5c9856da
Bogambe <bogambe@gmail.com>
2024-12-25 03:21:47 +0100

    All construct

---
+++ AlgoPractice/DynamicProgramming/AllConstruct.cs
@@ -0,0 +1,37 @@
+﻿namespace AlgoPractice.DynamicProgramming;
+
+// Find all of the ways to construct a word for given input
+public static class AllConstruct
+{
+    public static string[][] Run(string target, string[] words)
+    {
+        if (string.IsNullOrEmpty(target))
+        {
+            return [[]];
+        }
+
+        var result = new string[target.Length + 1][][];
+        result[0] = [[]];
+        // Initialize each first-level element with an empty array
+        for (int i = 1; i < result.Length; i++)
+        {
+            result[i] = [];
+        }
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            foreach (var word in words)
+            {
+                var targetIndex = i + word.Length;
+                if (targetIndex >= result.Length)
+                    continue;
+                if (target.Substring(i, word.Length) != word)
+                    continue;
+                var newCombinations = result[i].Select(subArray => subArray.Append(word).ToArray()).ToArray();
+                result[targetIndex] = result[targetIndex].Concat(newCombinations).ToArray();
+            }
+        }
+
+        return result[target.Length];
+    }
+}
---
+++ AlgoPracticeTest/DynamicProgramming/AllConstruct.cs
@@ -0,0 +1,30 @@
+﻿using AlgoPractice.DynamicProgramming;
+using System;
+using System.Collections.Generic;
+
+namespace AlgoPracticeTest.DynamicProgramming;
+
+public sealed class AllConstructTest
+{
+    [Theory]
+    [MemberData(nameof(GetTestData))]
+    public void ValidHappyFlow(string target, string[] words, string[][] expectedResult)
+    {
+        var result = AllConstruct.Run(target, words);
+        Assert.Equivalent(expectedResult, result);
+    }
+
+    public static IEnumerable<object[]> GetTestData()
+    {
+        yield return
+        [
+            "abcdef",
+            new[] { "ab", "abc", "cd", "def", "abcd", "ef", "c" },
+            new[] { new[] { "ab", "cd", "ef" }, new[] { "abcd", "ef" } }
+        ];
+
+        yield return ["", new[] { "cat", "dog" }, new[] { Array.Empty<string>() }];
+
+        yield return ["birds", new[] { "cat", "dog" }, Array.Empty<string>()];
+    }
+}
