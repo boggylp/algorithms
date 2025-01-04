9ea3d849089959d4907911f7311a285dd276fb7f
Bogambe <bogambe@gmail.com>
2025-01-04 03:48:55 +0100

    Linked List

---
+++ src/AlgoPractice/LinkedList/DeleteNode.cs
@@ -0,0 +1,10 @@
+namespace AlgoPractice.LinkedList;
+
+public static class DeleteNode
+{
+    public static void Run(ListNode node)
+    {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
---
+++ src/AlgoPractice/LinkedList/ListNode.cs
@@ -0,0 +1,8 @@
+namespace AlgoPractice.LinkedList;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
